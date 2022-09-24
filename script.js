7e9dfe4f870452828bd7ce6900e766d0e3d083b8
Rio Edwards <rioredwards@gmail.com>
2022-09-24 00:12:58 -0700

    Rendering multiple cards

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ import { renderCard } from "./card-render.js";
 
 const cardSectionEl = document.getElementById("cardSection");
 cardSectionEl.classList.add("card-section");
+let cardID = 0;
 
 function displayCard() {
-    cardSectionEl.innerHTML = "";
-
-    const cardEl = renderCard();
-    cardEl.style.setProperty("--card-height", "200px");
+    const cardEl = renderCard(cardID);
+    cardEl.style.setProperty("--card-height", "20rem");
     cardSectionEl.append(cardEl);
 }
-// for (let i = 0; i < 2; i++) {
-//     displayCard();
-// }
-displayCard();
+
+for (let i = 0; i < 6; i++) {
+    displayCard();
+    cardID++;
+}
